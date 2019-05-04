26ac96f2f6fc408ed91d9ad27c6bdb1e6cc38de8
成凡 <chengfan.yhb@alibaba-inc.com>
2019-05-04 14:45:48 +0800

    add postcss

--- index.js
+++ index.js
@@ -1,46 +1,70 @@
-const less = require('less');
-const CleanCssPlugin = require('less-plugin-clean-css');
-const fs = require('fs');
-const merge = require('lodash/merge');
+const less = require("less");
+const CleanCssPlugin = require("less-plugin-clean-css");
+const NpmImportPlugin = require("less-plugin-npm-import");
+const fs = require("fs");
+const { merge, isString } = require("lodash");
+const rucksack = require("rucksack-css");
+const autoprefixer = require("autoprefixer");
+const postcss = require("postcss");
+
+const postcssConfig = {
+  plugins: [
+    rucksack(),
+    autoprefixer({
+      browsers: ["last 2 versions", "Firefox ESR", "> 1%", "ie >= 9", "iOS >= 8", "Android >= 4"]
+    })
+  ]
+};
+
+function handleMainLess(mainLess) {
+  let content = "";
+  [].concat(mainLess).forEach(filePath => {
+    content += fs.readFileSync(filePath, "utf-8");
+    content = content.replace(/^\uFEFF/, "");
+    content += "\n";
+  });
+  return content;
+}
 
 class ThemePlugin {
   constructor(options) {
     const defaulOptions = {
-      filename: 'day.css',
-      mainLess: './node_modules/antd/dist/antd.less',
+      filename: "day.css",
+      // mainLess: './node_modules/antd/dist/antd.less',
+      mainLess: ["./node_modules/antd/lib/style/index.less", "./node_modules/antd/lib/style/components.less"],
       lessOptions: {
-        paths: ['./node_modules/antd/lib'],
+        paths: ["./node_modules/antd/lib"],
         javascriptEnabled: true,
         modifyVars: {},
-        plugins: [
-          new CleanCssPlugin({ advanced: true }),
-        ],
+        plugins: [new CleanCssPlugin({ advanced: true }), new NpmImportPlugin({ prefix: "~" })]
       }
     };
     this.options = merge(defaulOptions, options);
   }
 
   apply(compiler) {
-    compiler.hooks.emit.tapAsync('ThemePlugin', (compilation, callback) => {
+    compiler.hooks.emit.tapAsync("ThemePlugin", (compilation, callback) => {
       const { filename, mainLess, lessOptions } = this.options;
       try {
+        let data = handleMainLess(mainLess);
         less
-        .render(fs.readFileSync(mainLess).toString(), lessOptions)
-        .then((res) => {
-          const file = res.css;
-          compilation.assets[filename] = {
-            source() {
-              return file;
-            },
-            size() {
-              return file.length;
-            },
-          };
-          callback();
-        })
-        .catch((e) => {
-          callback(e);
-        });
+          .render(data, lessOptions)
+          .then(result => postcss(postcssConfig.plugins).process(result.css, { from: undefined }))
+          .then(res => {
+            const file = res.css;
+            compilation.assets[filename] = {
+              source() {
+                return file;
+              },
+              size() {
+                return file.length;
+              }
+            };
+            callback();
+          })
+          .catch(e => {
+            callback(e);
+          });
       } catch (err) {
         callback(err);
       }
